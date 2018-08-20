701f6fc1a0cfa9ac86aa26740b2b3d879d37839b
Fernando Rosa <fernandorosajr@gmail.com>
2018-08-19 21:28:51 -0300

    Alteracoes diversas

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-
-Meu TERCEIRO projeto no GIT
-
 Meu primeiro projeto no GIT
-Versao Teste
-
-Saluton 
--- js/js.js
+++ js/js.js
@@ -1,1 +1,6 @@
-alert("Testando GIT Hub!!!");
+/* alert("Testando GIT Hub!!!"); */
+var dssa; // Esta variavel eu criei de repente...
+dssa = "Minha variavel!";
+var nome = "Nando a nado";
+var nome2 = prompt("Qual o nome????"); 
+document.write("<h1>" + nome2 + "</h1>");
